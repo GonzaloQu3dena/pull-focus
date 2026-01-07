dbd08af72b8215ff622699c4986484f355b29275
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2026-01-06 22:13:47 -0500

    refactor(layout): update main layout to use project logo.

--- src/app/core/layouts/main-layout/main-layout.ts
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
