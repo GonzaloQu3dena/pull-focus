70d6ca52521f30342a984a9e38ac2b73e6cb0cd0
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2026-01-06 20:25:47 -0500

    feat(core): replace buttons with router links for navigation in main layout.

--- src/app/core/layouts/main-layout/main-layout.ts
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
