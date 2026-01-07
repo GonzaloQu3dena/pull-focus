32f3e63d442b25989f8f5c9866d81d23f039cbe3
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2026-01-06 19:28:10 -0500

    feat(shared): add custom button component.

---
+++ src/app/shared/presentation/components/custom-btn/custom-btn.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+
+export type ButtonVariant = 'primary' | 'danger' | 'outline' | 'ghost';
+
+@Component({
+  selector: 'app-custom-btn',
+  imports: [],
+  templateUrl: './custom-btn.html',
+  styleUrl: './custom-btn.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex',
+  },
+})
+export class CustomBtn {
+  /**
+   * Input to disable the button.
+   */
+  disabled = input<boolean>(false);
+  /**
+   * Input to set the button to full width.
+   */
+  fullWidth = input<boolean>(false);
+  /**
+   * Input to set the button variant.
+   */
+  variant = input<ButtonVariant>('primary');
+
+  /**
+   * Output to emit when the button is clicked.
+   */
+  btnClick = output<void>();
+
+  /**
+   * Method to handle the button click.
+   */
+  protected handleClick(): void {
+    if (!this.disabled()) {
+      this.btnClick.emit();
+    }
+  }
+}
