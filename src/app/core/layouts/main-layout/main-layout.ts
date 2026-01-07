a9943562a5f17413c2548a870d17b51032aebdad
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2026-01-06 19:29:17 -0500

    feat(core): create main layout component.

---
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainLayout {}
+
